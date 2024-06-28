beb5d3fa4b340bc907cc536c0e5ef7589521090b
Vadim <_s_e_t__@ukr.net>
2024-06-28 14:44:08 +0300

    added filder eventDetail

--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -1,4 +1,19 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../../dummy-data";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return <p>No Event found!</p>
+  }
+  
+ 
   return (
     <div>
       <h1>The Event Detaile Page</h1>
